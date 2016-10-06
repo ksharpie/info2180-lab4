d909acea0381abe01c0825b4254fea9ab193621a
Khalid Sharpe <sharpe.khalid@gmail.com>
2016-10-06 15:28:47 -0500

    exercise 3

--- maze.js
+++ maze.js
@@ -1,10 +1,22 @@
 function main() {
   
+  var failed = false;
+
   $(".boundary").hover( function() {
 
   	$("#maze .boundary").addClass("youlose");
 
+  	failed = true;
+
   });
+
+  $("#end").hover( function(){
+
+  	if ( failed == false ){
+  		alert('You win!');
+  	}
+
+  })
 
 }
 
