c170ea3aaf54a52454af14c27009020798bfe0c4
Khalid Sharpe <sharpe.khalid@gmail.com>
2016-10-14 16:53:15 -0500

    Exercise 2

--- maze.js
+++ maze.js
@@ -1,8 +1,17 @@
-window.onload = function(e){ 
-    var boundary1 = document.getElementById('boundary1');
+window.onload = function(e)
+{ 
+    
+  var boundaries = document.querySelectorAll('.boundary');
 
-    boundary1.onmouseover = function(){
-        boundary1.setAttribute('class', 'boundary youlose');
-    }
+  for (var i=0; i<boundaries.length; i++)
+  {
+    boundaries[i].onmouseover = function()
+    {
+      for (var j=0;j<boundaries.length-1; j++)
+      {
+        boundaries[j].setAttribute('class', 'boundary youlose');
+      }  
+    } 
+  }
 }
 
