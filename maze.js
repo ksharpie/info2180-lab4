f448a6922358eb64152aea90feb0bb29ab1c4fe3
Khalid Sharpe <sharpe.khalid@gmail.com>
2016-10-14 17:17:17 -0500

    Exercise 5

--- maze.js
+++ maze.js
@@ -1,34 +1,45 @@
 window.onload = function(e)
 { 
     
-  var boundaries = document.querySelectorAll('.boundary');
-  var end = document.getElementById('end');
-  var start = document.getElementById('start');
+  var boundaries = document.querySelectorAll(".boundary");
+  var end = document.getElementById("end");
+  var start = document.getElementById("start");
+  var status = document.getElementById("status");
+
+  var gameStatus = "normal";
 
   for (var i=0; i<boundaries.length; i++)
   {
     boundaries[i].onmouseover = function()
     {
-      for (var j=0;j<boundaries.length-1; j++)
-      {
-        boundaries[j].setAttribute('class', 'boundary youlose');
-      }  
-    } 
+      if(gameStatus == "normal")  
+      { 
+        for (var j=0;j<boundaries.length-1; j++)
+        {
+          boundaries[j].setAttribute("class", "boundary youlose");
+        }
+        gameStatus = "Lost";
+        status.innerHTML = "You Lose!"; 
+      }
+    }
   }
 
   start.onclick = function()
   {
     for (var i=0;i<boundaries.length-1; i++)
     {
-      boundaries[i].setAttribute('class', 'boundary');
+      boundaries[i].setAttribute("class", "boundary");
+      status.innerHTML = "Move your mouse over the \"S\" to begin.";
     }
   }
 
   end.onmouseover = function()
   {
-      alert("You Win!"); 
+    if(gameStatus == "normal")
+    {
+      status.innerHTML = "You Win!";
+      gameStatus = "Won";
+    }
   }
-
-
 }
 
